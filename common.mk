0bd9cc1578741f3ed4d382e8533c7c7271d7e797
micky387 <mickaelsaibi@free.fr>
2024-07-04 20:51:08 -0400

    universal2100-common: Build libstagefright_softomx_plugin libraries
    E vndksupport: Could not load libstagefright_softomx_plugin.so from default namespace: dlopen failed: library "libstagefright_softomx_plugin.so" not found.
    
    Change-Id: I4b21476fc04df2acc89f2a34035b1642f2176f7f

--- common.mk
+++ common.mk
@@ -175,6 +175,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
+PRODUCT_PACKAGES += \
+    libstagefright_softomx_plugin.vendor
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(COMMON_PATH)/overlay
 PRODUCT_ENFORCE_RRO_TARGETS := *
