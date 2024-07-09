f08458c3cad9e4427a9511c58ae23ad1bb0ce73f
Tim Zimmermann <tim@linux4.de>
2024-07-09 10:37:38 -0400

    universal2100-common: Switch to AIDL camera HAL

--- common.mk
+++ common.mk
@@ -91,7 +91,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.6-service_64.samsung \
+    android.hardware.camera.provider-service.samsung \
     libhypervintf \
     libsensorndkbridge
 
